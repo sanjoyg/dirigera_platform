bbaaf448fb0ac87377502d6eca482df95d1107ab
Sanjoy Ghosh <c503ghosh@gmail.com>
2024-04-30 09:11:36 +0200

    Fixed bugs after hasty initial commit

--- custom_components/dirigera_platform/binary_sensor.py
+++ custom_components/dirigera_platform/binary_sensor.py
@@ -55,33 +55,32 @@ async def async_setup_entry(
         logger.debug("Found {} motion_sensor entities to setup...".format(len(motion_sensors)/2))
         async_add_entities(motion_sensors)
     
-        if 1==2:
-            hub_open_close_sensors : list[OpenCloseSensor] = await hass.async_add_executor_job(hub.get_open_close_sensors)
-            open_close_devices : list[ikea_open_close_device] = [
-                ikea_open_close_device(hass, hub, open_close_sensor)
-                for open_close_sensor in hub_open_close_sensors
-            ]
+        hub_open_close_sensors : list[OpenCloseSensor] = await hass.async_add_executor_job(hub.get_open_close_sensors)
+        open_close_devices : list[ikea_open_close_device] = [
+            ikea_open_close_device(hass, hub, open_close_sensor)
+            for open_close_sensor in hub_open_close_sensors
+        ]
 
-            open_close_sensors = []
-            for device in open_close_devices:
-                open_close_sensors.append(ikea_motion_sensor(device))
-                open_close_sensors.append(battery_percentage_sensor(device))
+        open_close_sensors = []
+        for device in open_close_devices:
+            open_close_sensors.append(ikea_open_close(device))
+            open_close_sensors.append(battery_percentage_sensor(device))
 
-            logger.debug("Found {} open close entities to setup...".format(len(open_close_sensors)/2))
-            async_add_entities(open_close_sensors)
+        logger.debug("Found {} open close entities to setup...".format(len(open_close_sensors)/2))
+        async_add_entities(open_close_sensors)
 
-            hub_water_sensors : list[WaterSensor] = await hass.async_add_executor_job(hub.get_water_sensors)
-            water_sensor_devices = [ ikea_water_sensor_device(hass, hub, hub_water_sensor) 
-                                    for hub_water_sensor in hub_water_sensors
-                                ]
-            
-            water_sensors = []
-            for device in water_sensor_devices:
-                water_sensors.append(battery_percentage_sensor(device))
-                water_sensors.append(ikea_water_sensor(device))
+        hub_water_sensors : list[WaterSensor] = await hass.async_add_executor_job(hub.get_water_sensors)
+        water_sensor_devices = [ ikea_water_sensor_device(hass, hub, hub_water_sensor) 
+                                for hub_water_sensor in hub_water_sensors
+                            ]
+        
+        water_sensors = []
+        for device in water_sensor_devices:
+            water_sensors.append(battery_percentage_sensor(device))
+            water_sensors.append(ikea_water_sensor(device))
 
-            logger.debug(f"Found {len(hub_water_sensors)/2} water sensors to setup....")
-            async_add_entities(water_sensors)
+        logger.debug(f"Found {len(hub_water_sensors)/2} water sensors to setup....")
+        async_add_entities(water_sensors)
 
     logger.debug("Binary Sensor Complete async_setup_entry")
 
@@ -103,17 +102,21 @@ class ikea_motion_sensor(ikea_base_device_sensor, BinarySensorEntity):
 class ikea_open_close_device(ikea_base_device):
     def __init__(self, hass, hub, json_data):
         logger.debug("ikea_motion_sensor_device ctor...")
-        super().__init__(hub, hass, json_data, hub.get_open_close_by_id)
+        super().__init__(hass, hub, json_data, hub.get_open_close_by_id)
 
 class ikea_open_close(ikea_base_device_sensor, BinarySensorEntity):
-    def __init__(self, device: ikea_motion_sensor_device):
+    def __init__(self, device: ikea_open_close_device):
         logger.debug("ikea_motion_sensor ctor...")
         # No suffix or name prefix for backward compatibility
-        super(ikea_base_device_sensor).__init__(device)
+        super().__init__(device)
 
     @property
     def device_class(self) -> str:
         return BinarySensorDeviceClass.WINDOW
+
+    @property
+    def is_on(self):
+        return self._device.is_open
 
 class ikea_water_sensor_device(ikea_base_device):
     def __init__(self, hass, hub, json_data):
