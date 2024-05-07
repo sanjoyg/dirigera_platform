34fa4f2f53e8ad41f253c0901f646cae0c5e0b08
Sanjoy Ghosh <c503ghosh@gmail.com>
2024-05-07 07:51:21 -0400

    reverted scene.py

--- custom_components/dirigera_platform/scene.py
+++ custom_components/dirigera_platform/scene.py
@@ -10,13 +10,14 @@ from homeassistant.components.scene import Scene
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_IP_ADDRESS, CONF_TOKEN
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
 from .icons import to_hass_icon
-from .base_classes import ikea_base_device
 
 logger = logging.getLogger("custom_components.dirigera_platform")
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -30,25 +31,47 @@ async def async_setup_entry(
         return
 
     scenes: list[DirigeraScene] = await hass.async_add_executor_job(hub.get_scenes)
-    entities: list[IkeaScene] = [IkeaScene(hass, hub, s) for s in scenes]
+    entities: list[IkeaScene] = [IkeaScene(hub, s) for s in scenes]
     logger.debug("Found %d scenes", len(entities))
     async_add_entities(entities)
 
 
-class IkeaScene(ikea_base_device, Scene):
+class IkeaScene(Scene):
     """Implements scene entity for a Dirigera scene."""
 
     _attr_has_entity_name = True
 
-    def __init__(self, hass: HomeAssistant, hub: Hub, dirigera_scene: DirigeraScene) -> None:
-        logger.debug("IkeaScena ctor...")
-        super().__init__(hass, hub, dirigera_scene, hub.get_scene_by_id)
+    def __init__(self, hub: Hub, dirigera_scene: DirigeraScene) -> None:
+        """Initialize."""
+        self._hub = hub
+        self._dirigera_scene = dirigera_scene
+        self._attr_unique_id = dirigera_scene.id
 
+    @property
+    def name(self) -> str:
+        """Return name from Dirigera."""
+        return self._dirigera_scene.info.name
+
+    @property
     def icon(self) -> str:
         """Return suitable replacement icon."""
-        return to_hass_icon(self._json_data.info.icon)
+        return to_hass_icon(self._dirigera_scene.info.icon)
 
     async def async_activate(self, **kwargs: Any) -> None:
         """Trigger Dirigera Scene."""
         logger.debug("Activating scene '%s' (%s)", self.name, self.unique_id)
         await self.hass.async_add_executor_job(self._dirigera_scene.trigger)
+
+    async def async_update(self) -> None:
+        """Fetch updated scene definition from Dirigera."""
+        logger.debug("Updating scene '%s' (%s)", self.name, self.unique_id)
+        try:
+            self._dirigera_scene = await self.hass.async_add_executor_job(
+                self._hub.get_scene_by_id, self.unique_id
+            )
+        except Exception as ex:
+            logger.error(
+                "Error encountered on update of '%s' (%s)", self.name, self.unique_id
+            )
+            logger.error(ex)
+            raise HomeAssistantError from ex
