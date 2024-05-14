1a4ec7f4e07b3fd32e53d9dc502578d9367da148
slajob <slawekjob174@gmail.com>
2024-05-14 16:04:38 +0200

    update TriggerDetails field reference to prevent pydantic validation error

--- custom_components/dirigera_platform/scene.py
+++ custom_components/dirigera_platform/scene.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from dirigera import Hub
 from dirigera.devices.scene import Scene as DirigeraScene
-from dirigera.devices.scene import Trigger
+from dirigera.devices.scene import Trigger, TriggerDetails
 
 from homeassistant.components.scene import Scene
 from homeassistant.config_entries import ConfigEntry
@@ -28,6 +28,7 @@ async def async_setup_entry(
     hub = Hub(config[CONF_TOKEN], config[CONF_IP_ADDRESS])
 
     Trigger.update_forward_refs()
+    TriggerDetails.update_forward_refs()
 
     # TODO: Add mock scenes
     if config[CONF_IP_ADDRESS] == "mock":
