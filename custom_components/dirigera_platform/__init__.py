40ad0073d2865eeba728f4967386480276f6dd9f
Colin de Vries <colindv2000@gmail.com>
2024-10-06 13:27:32 +0200

    These shouldn't be error loggings.

--- custom_components/dirigera_platform/__init__.py
+++ custom_components/dirigera_platform/__init__.py
@@ -35,10 +35,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 hub_events = None 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    logger.error("Starting async_setup...")
+    logger.debug("Starting async_setup...")
     for k in config.keys():
-        logger.error(f"config key: {k} value: {config[k]}")
-    logger.error("Complete async_setup...")
+        logger.debug(f"config key: {k} value: {config[k]}")
+    logger.debug("Complete async_setup...")
 
     def handle_dump_data(call):
         import dirigera
@@ -70,9 +70,9 @@ async def async_setup_entry(
 ) -> bool:
     global hub_events
     """Set up platform from a ConfigEntry."""
-    logger.error("Staring async_setup_entry in init...")
-    logger.error(dict(entry.data))
-    logger.error(f"async_setup_entry {entry.unique_id} {entry.state} {entry.entry_id} {entry.title} {entry.domain}")
+    logger.debug("Staring async_setup_entry in init...")
+    logger.debug(dict(entry.data))
+    logger.debug(f"async_setup_entry {entry.unique_id} {entry.state} {entry.entry_id} {entry.title} {entry.domain}")
     hass.data.setdefault(DOMAIN, {})
     hass_data = dict(entry.data)
 
@@ -139,15 +139,15 @@ async def async_unload_entry(
     hub = Hub(hass_data[CONF_TOKEN], hass_data[CONF_IP_ADDRESS])
     
     # For each controller if there is an empty scene delete it
-    logger.error("In unload so forcing delete of scenes...")
+    logger.debug("In unload so forcing delete of scenes...")
     scenes: list[DirigeraScene] = await hass.async_add_executor_job(hub.get_scenes)
     for scene in scenes:
         if scene.info.name is None or not scene.info.name.startswith("dirigera_platform_empty_scene_"):
-            logger.error(f"Ignoring scene : {scene.info.name}, as not empty scene")
+            logger.debug(f"Ignoring scene : {scene.info.name}, as not empty scene")
             continue
-        logger.error(f"Deleting scene {scene.id}...")
+        logger.debug(f"Deleting scene {scene.id}...")
         await hass.async_add_executor_job(hass.delete_scene,scene.id)
-    logger.error("Done deleting scene....")
+    logger.debug("Done deleting scene....")
     
     """Unload a config entry."""
     unload_ok = all(
